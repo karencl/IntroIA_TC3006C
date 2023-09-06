db1474af7bb10d8991fa0a42c31c7cff7fc71909
karencl <69648059+karencl@users.noreply.github.com>
2023-09-06 16:08:39 -0600

    Listo

---
+++ Entrega2/README.md
@@ -0,0 +1,30 @@
+## Técnica utilizado
+Para la segunda entrega del módulo 2, decidí implementar KMeans para el agrupamiento de los datos del dataset que elegí.
+
+## Dataset
+El dataset que utilicé para este entregable se llama "Seeds", obtenido de: https://archive.ics.uci.edu/dataset/236/seeds.
+Lo que se busca con este data set es agrupar tres tipos diferentes de trigo: Kama, Rosa and Canadian. Esto se hace mediante los datos que se obtienen (área, perímetro, compacidad, largo, ancho, etc), al analizar el núcleo de éstos, mediante una técnica de rayos x.
+
+Especificaciones:
+- 210 datos
+- Multivariable
+- No. de atributos: 7
+- Tipo de artibutos: Reales
+- No. de grupos: 3
+
+## Especificaciones
+Para la visualización de los datos, apliqué la técnica de PCAs, para poder visualizar todas las entradas del dataset de manera representativa en una sola gráfica.
+***(NOTA: esta técnica la utilicé meramente para visualización y no para entrenar al modelo)***
+
+
+## Observaciones de los resultados
+
+- **Gráfica 1 de los PCAs (grupos reales):** En esta gráfica se puede observar el dataset completo de manera representativa, utilizando la técnica de PCAs. En esta podemos ver cuales son los 3 grupos reales y como los datos se distribuyen entre ellos.
+
+- **Gráfica 2 de los PCAs (predicciones):** En esta gráfica también se puede observar el dataset completo de manera representativa, utilizando la técnica de PCA. Sin embargo, la diferencia que tiene ésta con la gráfica pasada, es que podemos ver cuales son los 3 grupos que se predijeron y como los datos que se asignaron a cada uno de ellos se distribuyen.
+
+Podemos observar que los grupos que se predijeron (junto con los datos que hay en cada uno de ellos), son bastante similares a los grupos reales (y sus datos) mostrados en la gráfica 1 de los PCAs; por lo que se puede decir que el uso del método 'KMeans' cumplió con el objetivo que se buscaba en esta práctica.
+
+- **Gráfica del 'elbow method':** En esta gráfica se pueden observar el resultado obtenido al utilizar la técnica 'elbow method', que sirve para evaluar el número adecuado de clusters que deberían de haber para este dataset.
+
+Como podemos ver, el 'elbow' está prácticamente en 3.
---
+++ Entrega2/main.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
+
+def plotPCAs(pca_results, labels, final_title):
+    """ Función para graficar los PCAs que representan todo el dataframe original
+    Args:
+        pca_results (DataFrame) - Dataframe con los 2 PCAs creados para la
+                                  representación de los datos.
+        labels (series) - serie que contiene los valores de los grupos reales
+        final_title (string) - string que contiene una extensión para la última
+                               parte del título de la gráfica  
+    """
+    
+    plt.figure(figsize=(10,5))
+    sns.scatterplot(x="pca1", y="pca2", hue=labels, data=pca_results)
+    plt.title('K-means Clustering with 2 dimensions' + final_title)
+    
+
+def evaluateNumClusters():
+    """ Función que utiliza 'elbow method' para evaluar el número apropiado de
+        clusters que se deben de tener para este dataset
+    """
+    
+    kmeans_kwargs = {
+    "init": "random",
+    "n_init": 10,
+    "max_iter": 300,
+    "random_state": 42,
+    }
+
+    sse = []
+    for k in range(1, 11):
+        kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+        kmeans.fit(inputs)
+        sse.append(kmeans.inertia_)
+    
+    plt.figure(figsize=(10,5))   
+    plt.style.use("fivethirtyeight")
+    plt.plot(range(1, 11), sse)
+    plt.xticks(range(1, 11))
+    plt.xlabel("Number of Clusters")
+    plt.ylabel("SSE")
+    plt.title("Elbow method")
+
+
+if __name__ == '__main__':
+    # Carga y manejo de datos
+    seeds = pd.read_csv('seeds_dataset.csv')
+    seeds
+
+    inputs = seeds[['Area', 'Perimeter', 'Compactness', 
+                    'Length of kernel', 'Width of kernel', 
+                    'Asymmetry coefficient', 'Length of kernel grove']]
+    labels = seeds[['Class']]
+
+    # Creación de 2 PCAs para la representación del dataframe
+    reduced_data = PCA(n_components = 2).fit_transform(inputs)
+    results = pd.DataFrame(reduced_data,columns=['pca1','pca2'])
+
+    # Se muestran nuevamente los datos con los agrupamientos reales
+    plotPCAs(results, labels['Class'], " - GRUPOS REALES")
+
+    # Inicialización de KMeans (3 clusters)
+    kmeans = KMeans(
+        n_clusters = 3,
+        init="random",
+        n_init=10,
+        max_iter=300,
+        random_state=42
+    )
+    
+    # Fit para los inputs
+    kmeans.fit(inputs)
+    
+    # E valor SSE más bajo
+    print(kmeans.inertia_)
+    # Posiciones finales de los centros
+    print(kmeans.cluster_centers_)
+    
+    # Predicciones de agrupamiento
+    y = kmeans.predict(inputs)
+    
+    # Se muestran nuevamente los datos, pero ahora con las predicciones
+    plotPCAs(results, y, " - PREDICCIONES")
+    
+    # elbow method
+    evaluateNumClusters()
+    
+    plt.show()
